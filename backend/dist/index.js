32c02695f1f563337de74ea062d84ba1f1f60fae
Itgelt Gereltsogt <itgelt@Itgelts-MacBook-Air.local>
2025-10-18 18:40:12 +0800

    debug

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -4,16 +4,28 @@ import "dotenv/config";
 import { connectDB } from "./database/index.js";
 import { orderRouter } from "./routes/order.router.js";
 import { userRouter } from "./routes/user.router.js";
+
 const app = express();
+
 app.use(express.json());
+
 const port = 8000;
-app.use(cors());
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://cargo-system-hjqk.vercel.app"],
+    credentials: true,
+  })
+);
+
 app.use("/order", orderRouter);
 app.use("/user", userRouter);
+
 const server = async () => {
-    await connectDB();
-    app.listen(port, () => {
-        console.log(`server running at http://localhost:${port}/`);
-    });
+  await connectDB();
+  app.listen(port, () => {
+    console.log(`server running at http://localhost:${port}/`);
+  });
 };
+
 server();
